7f7d270299093acf34ca3b54a52f2d7f7b339468
yangyanbo <yangyanbo@fuzhi.ai>
2023-10-25 14:23:24 +0800

    feat: 代码生成 string

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,6 +1,11 @@
-export function transform(root, options) {
+export function transform(root, options = {}) {
   const context = createTransformContext(root, options);
   transformNode(root, context);
+  createCodegen(root);
+}
+
+function createCodegen(root) {
+  root.codegenNode = root.children[0];
 }
 
 function createTransformContext(root, options) {
