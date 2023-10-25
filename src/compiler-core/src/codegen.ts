fe3ce30244485f5975b090a9b62e41927ab1136e
yangyanbo <yangyanbo@fuzhi.ai>
2023-10-25 15:48:26 +0800

    feat: 代码生成 插值表达式

--- src/compiler-core/src/codegen.ts
+++ src/compiler-core/src/codegen.ts
@@ -1,9 +1,12 @@
+import { NodeTypes } from './act';
+import { TO_DISPLAY_STRING, helperNameMap } from './runtimeHelpers';
+
 export function generate(ast) {
   const context = createCodegenContext(ast);
 
   const { push } = context;
 
-  push('return ');
+  genFunctionPreamble(ast, context);
 
   const functionName = 'render';
   const args = ['_ctx', '_cache'];
@@ -22,17 +25,63 @@ export function generate(ast) {
   };
 }
 
+function genFunctionPreamble(ast, context) {
+  const { push } = context;
+  const VueBinding = 'Vue';
+
+  const helpers = ast.helpers;
+  const aliasHelper = (s) => `${helperNameMap[s]}: _${helperNameMap[s]}`;
+
+  if (helpers.length > 0) {
+    push(`const { ${helpers.map(aliasHelper).join(', ')} } = ${VueBinding}`);
+  }
+  push('\n');
+
+  push('return ');
+}
+
 function createCodegenContext(ast: any) {
   const context = {
     code: '',
     push(source) {
       context.code += source;
     },
+    helper(k) {
+      return `_${helperNameMap[k]}`;
+    },
   };
   return context;
 }
 
 function genNode(node: any, context: any) {
+  switch (node.type) {
+    case NodeTypes.TEXT:
+      genText(node, context);
+      break;
+    case NodeTypes.INTERPOLATION:
+      genInterpolation(node, context);
+      break;
+    case NodeTypes.SIMPLE_EXPRESSION:
+      genExpression(node, context);
+      break;
+    default:
+      break;
+  }
+}
+
+function genText(node: any, context: any) {
   const { push } = context;
   push(`'${node.content}'`);
+}
+
+function genInterpolation(node: any, context: any) {
+  const { push, helper } = context;
+  push(`${helper(TO_DISPLAY_STRING)}(`);
+  genNode(node.content, context);
+  push(')');
+}
+
+function genExpression(node: any, context: any) {
+  const { push } = context;
+  push(node.content);
 }
--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,7 +1,11 @@
+import { NodeTypes } from './act';
+import { TO_DISPLAY_STRING } from './runtimeHelpers';
+
 export function transform(root, options = {}) {
   const context = createTransformContext(root, options);
   transformNode(root, context);
   createCodegen(root);
+  root.helpers = Array.from(context.helpers.keys());
 }
 
 function createCodegen(root) {
@@ -9,10 +13,15 @@ function createCodegen(root) {
 }
 
 function createTransformContext(root, options) {
-  return {
+  const context = {
     root,
     nodeTransforms: options.nodeTransforms || [],
+    helpers: new Map(),
+    helper(key) {
+      context.helpers.set(key, 1);
+    },
   };
+  return context;
 }
 
 function transformNode(node, context) {
@@ -22,7 +31,20 @@ function transformNode(node, context) {
     transform(node);
   }
 
-  const children = node.children || [];
+  switch (node.type) {
+    case NodeTypes.INTERPOLATION:
+      context.helper(TO_DISPLAY_STRING);
+      break;
+    case NodeTypes.Root:
+    case NodeTypes.ELEMENT:
+      transformChildren(node, context);
+    default:
+      break;
+  }
+}
+
+function transformChildren(node: any, context: any) {
+  const children = node.children;
 
   for (const child of children) {
     transformNode(child, context);
